59f117da80338013c454e24caee529bd8a02ef6f
njspataro <60331948+njspataro@users.noreply.github.com>
2020-03-22 16:20:45 -0500

    week10

--- Hotel_Camden/config.js
+++ Hotel_Camden/config.js
@@ -1,4 +1,10 @@
 $(document).bind("mobileinit",  function() {
 	$.mobile.pageLoadErrorMessage = "oops, something went wrong.";
 });
+$(document).bind("mobileinit",  function() {
+	$.mobile.defaultPageTransition = "none";
+});
+$(document).bind("mobileinit",  function() {
+	$.mobile.keepNative = "input.boring";
+});
 
